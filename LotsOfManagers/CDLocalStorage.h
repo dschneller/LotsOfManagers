97e1d8b57731e83df52d4e7e63378f1f896632e3
predrag.karic <predrag.karic@codecentric.de>
2012-09-27 16:42:08 +0200

    document added to cd datamodel

--- LotsOfManagers/CDLocalStorage.h
+++ LotsOfManagers/CDLocalStorage.h
@@ -11,4 +11,5 @@
 
 @interface CDLocalStorage : NSObject
 
+
 @end
---
+++ LotsOfManagers/DocumentCD.m
@@ -0,0 +1,21 @@
+//
+//  DocumentCD.m
+//  LotsOfManagers
+//
+//  Created by Predrag Karic on 9/27/12.
+//  Copyright (c) 2012 codecentric AG. All rights reserved.
+//
+
+#import "DocumentCD.h"
+
+
+@implementation DocumentCD
+
+@dynamic filename;
+@dynamic author;
+@dynamic version;
+@dynamic timestamp;
+@dynamic documentId;
+@dynamic previewPath;
+
+@end
